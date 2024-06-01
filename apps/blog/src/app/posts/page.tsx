a45bdeea40bbdd62aa5189aa467a17aa13b3c75f
ateals <ateals@icloud.com>
2024-06-01 22:24:30 +0900

    feat: Post Toggle생성

--- apps/blog/src/app/posts/page.tsx
+++ apps/blog/src/app/posts/page.tsx
@@ -1,23 +1,27 @@
 import { Separator } from "@/components/ui";
 import { groupPostsByYear } from "@/service/mdx";
-import { DocumentBuilder, allPosts } from "@/service/mdx/post";
+import { DocumentBuilder } from "@/service/mdx/post";
 import { Header } from "@/widgets/Header";
 import { YearGroupPostList } from "@/widgets/posts/PostList";
+import { PostsToggle } from "@/widgets/posts/PostsToggle";
 
-export default function Page({ searchParams }: { searchParams: { type: "docs" | "post" } }) {
-  const posts = new DocumentBuilder().query({ type: searchParams["type"] }).getDocuments();
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { type?: "docs" | "post"; section?: string; tags?: string };
+}) {
+  const query = { ...searchParams, tags: searchParams.tags?.split(",") || [] };
+
+  const posts = new DocumentBuilder().query(query).getDocuments();
 
   const groupedPosts = groupPostsByYear(posts);
-
-  console.log(
-    searchParams["type"],
-    posts.map((post) => post.title)
-  );
 
   return (
     <div className="mx-auto max-w-xl py-8 pt-40 text-gray-700 dark:text-gray-300">
       <Header title="Ateals">
         <p>기술 블로그</p>
+
+        <PostsToggle type={searchParams.type} />
       </Header>
 
       <Separator />
