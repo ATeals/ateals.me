92c5d5639c851be362fcd6517800aa4f76eced9f
ateals <ateals@icloud.com>
2024-06-05 15:26:52 +0900

    style: mdx img 태그 크기 변경

--- apps/blog/src/components/MDX/IMGComponenets.tsx
+++ apps/blog/src/components/MDX/IMGComponenets.tsx
@@ -5,7 +5,7 @@ export const IMGComponenets = (props: HTMLProps<HTMLImageElement>) => (
     <img
       src={props.src}
       alt={props.alt}
-      className="shadow-lg object-contain object-center w-2/3 mb-2"
+      className="shadow-lg object-contain object-center mx-10 max-h-dvh mb-2"
     />
     <span className="text-center text-sm text-gray-500">{props.alt}</span>
   </span>
