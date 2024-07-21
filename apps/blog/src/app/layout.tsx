aadb536c9c975a3c8bda09ef27c3dc441439e8a3
ATeals <ateals@icloud.com>
2024-07-21 22:38:01 +0900

    fix : main bg-white
    기본 bg도 설정해야 darkmode transition 에니메이션이 정상적으로 동작함

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -20,23 +20,20 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-        <meta
-          name="google-site-verification"
-          content="U3213RTjnL2MBwyxUP3W66hcIIqcwV24irUIYoSkKzQ"
-        />
+        <meta name="google-site-verification" content="U3213RTjnL2MBwyxUP3W66hcIIqcwV24irUIYoSkKzQ" />
       </head>
 
       <link rel="alternate" type="application/rss+xml" href="/rss" title="RSS" />
       <link rel="alternate" type="application/atom+xml" href="/atom" title="RSS Atom" />
       <link rel="alternate" type="application/json" href="/json" title="JSON Feed" />
-      <body
-        className="dark:bg-dark-bg transition-colors ease-in-out duration-500"
-        style={GeistSans.style}
-      >
+      <body className="" style={GeistSans.style}>
         <BlurSection />
-        <main className="min-h-dvh">
-          <ThemeProvider>{children}</ThemeProvider>
-        </main>
+
+        <ThemeProvider>
+          <main className="min-h-dvh bg-white dark:bg-dark-bg transition-colors ease-in-out duration-500">
+            {children}
+          </main>
+        </ThemeProvider>
 
         <Footer />
 
--- apps/blog/src/widgets/providers/ThemeProvider.tsx
+++ apps/blog/src/widgets/providers/ThemeProvider.tsx
@@ -1,12 +1,7 @@
 import { ThemeProvider as Provider } from "next-themes";
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => (
-  <Provider
-    attribute="class"
-    enableSystem
-    defaultTheme="system"
-    themes={["light", "dark", "system"]}
-  >
+  <Provider attribute="class" enableSystem defaultTheme="system" themes={["light", "dark", "system"]}>
     {children}
   </Provider>
 );
