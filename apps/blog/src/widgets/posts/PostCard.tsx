b43e7ed5744be5f3b2ea6e1c782e71f54500818b
ATeals <ateals@icloud.com>
2024-09-09 16:06:30 +0900

    refactor(blog): Masonry를 통한 CardlistView

--- apps/blog/src/widgets/posts/PostCard.tsx
+++ apps/blog/src/widgets/posts/PostCard.tsx
@@ -3,7 +3,7 @@
 import { Post } from "@/components/Post";
 import { cn } from "@/lib/utils";
 import { Document } from "@/service/mdx/post";
-import { HTMLProps, useEffect, useRef } from "react";
+import { Children, HTMLProps, ReactNode, useEffect, useRef } from "react";
 
 interface PostCardProps extends HTMLProps<HTMLDivElement> {
   post: Document;
@@ -16,21 +16,11 @@ interface PostCardListProps extends HTMLProps<HTMLDivElement> {
 export const PostCard = ({ post, className, ...props }: PostCardProps) => {
   const tw = cn("px-[0.125rem] hover:cursor-pointer", className);
 
-  const ref = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const item = ref.current;
-
-    if (!item) return;
-
-    item.style.gridRowEnd = `span ${item.clientHeight}`;
-  }, [ref.current]);
-
   return (
-    <div ref={ref} className={tw} {...props}>
+    <div className={tw} {...props}>
       <Post.Provider post={post}>
-        <Post.Link className="block">
-          <div className="rounded-lg overflow-hidden">
+        <Post.Link className="block ">
+          <div className="rounded-lg overflow-hidden hover:border-2 hover:border-primary-sm">
             <Post.Image className="group-hover/post:scale-110 duration-300" />
           </div>
           <div className="px-2 py-1">
@@ -47,12 +37,38 @@ export const PostCard = ({ post, className, ...props }: PostCardProps) => {
 };
 
 export const PostCardList = ({ posts, className, ...props }: PostCardListProps) => {
-  const tw = cn("grid box-border grid-cols-2", className);
+  return (
+    <Masonry className="w-full">
+      {posts.map((post) => (
+        <PostCard post={post} key={post._id} />
+      ))}
+    </Masonry>
+  );
+};
+
+interface MasonryProps extends HTMLProps<HTMLDivElement> {
+  columnCount?: number;
+}
+
+const Masonry = ({ children, columnCount = 2, ...props }: MasonryProps) => {
+  const childrenArray = Children.toArray(children);
+
+  const columns = Array.from({ length: columnCount }, (_, index) =>
+    childrenArray.filter((_, childIndex) => childIndex % columnCount === index)
+  );
+
+  const tw = cn("flex flex-wrap -mx-2", props.className);
 
   return (
     <div className={tw}>
-      {posts.map((post, index) => (
-        <PostCard post={post} key={post._id} />
+      {columns.map((column, columnIndex) => (
+        <div key={columnIndex} className="w-full sm:w-1/2 md:w-1/2 px-">
+          {column.map((item, itemIndex) => (
+            <div key={itemIndex} className="mb-4">
+              {item}
+            </div>
+          ))}
+        </div>
       ))}
     </div>
   );
