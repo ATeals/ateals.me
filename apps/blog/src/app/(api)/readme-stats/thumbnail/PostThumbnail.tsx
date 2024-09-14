ac96bbdb73a0dbaa12fa75a0f3f49dacf90052fe
ATeals <ateals@icloud.com>
2024-09-15 03:25:32 +0900

    fix(blog) : route.ts로 변경

--- apps/blog/src/app/(api)/readme-stats/thumbnail/PostThumbnail.tsx
+++ apps/blog/src/app/(api)/readme-stats/thumbnail/PostThumbnail.tsx
@@ -1,25 +1,4 @@
-import { DocumentBuilder } from "@/service/mdx";
-import { NextRequest } from "next/server";
-
-import { ImageResponse } from "@vercel/og";
-
-export const GET = async (req: NextRequest) => {
-  const searchParams = req.nextUrl.searchParams;
-
-  const type = searchParams.get("type") || "Blog";
-  const index = parseInt(searchParams.get("index") || "0", 10);
-  const title = searchParams.get("title");
-
-  const post = title
-    ? new DocumentBuilder().getDocuments().find((post) => post.title === title)
-    : new DocumentBuilder().query({ type: type as any }).getDocuments()[index];
-
-  if (!post) return new Response("Not Found", { status: 404 });
-
-  const area = { width: 300, height: 400 };
-
-  return new ImageResponse(<PostThumbnail post={post} area={area} />, area);
-};
+import React from "react";
 
 export const PostThumbnail = ({ post, area }: { post: any; area: { width: number; height: number } }) => {
   const imageWidth = area.width;
