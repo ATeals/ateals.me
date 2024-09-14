363a0f40721d677f77ea475307fe70356ab327a1
ATeals <ateals@icloud.com>
2024-09-15 03:45:45 +0900

    fix(blog) : robots.txt disallow "readme-stats"

--- apps/blog/src/app/robots.ts
+++ apps/blog/src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: ["/private/", "/readme-stats/"],
     },
     sitemap: `${SITE_CONFIG.domain}/sitemap.xml`,
   };
