bf335131134ef02639afd198c12691791634e81d
ATeals <ateals@icloud.com>
2024-12-29 01:20:37 +0900

    refactor: 불필요한 의존성 제거 및 이미지 컴포넌트 개선

--- apps/blog/src/components/MDX/IMG.tsx
+++ apps/blog/src/components/MDX/IMG.tsx
@@ -1,10 +1,24 @@
-export const IMG = ({ src, alt, title }: { src: string; alt: string; title?: string }) => {
+import { cn } from '@repo/shadcn/utils';
+
+export const IMG = ({
+  src,
+  alt,
+  title,
+  ...props
+}: {
+  src: string;
+  alt: string;
+  title?: string;
+  className?: string;
+}) => {
   const caption = alt.includes('caption:') ? alt.split('caption:')[1] : null;
 
-  return (
-    <div className="mx-auto">
+  return caption ? (
+    <div {...props} className={cn('mx-auto', props.className)}>
       <img src={src} alt={alt} title={title} className="my-0" />
-      {caption && <p className="mt-0 text-center text-sm text-gray-400">{caption}</p>}
+      <p className="mt-0 text-center text-sm text-gray-400">{caption}</p>
     </div>
+  ) : (
+    <img src={src} alt={alt} title={title} {...props} className={cn(props.className)} />
   );
 };
